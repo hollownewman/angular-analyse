03d8149cfa65a1bf34ad9a3737e8e71dfcbeeff9
hollownewman <pingfan5922@gmail.com>
2017-08-31 22:40:38 +0800

    部分修改

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,17 @@
-var loginModule = angular.module('loginModule',[])
-loginModule.controller('iformController',['$scope',function($scope){
+var loginModule = angular.module('loginModule',[]);
+var log = [];
+loginModule.controller('iformController',function($scope,$http){
+  // var values = {name:'misko',gender:'male'};
+  // angular.forEach(values,function(value,key){
+  //   console.log(value+'z');
+  //   console.log(this==log);
+  //   this.push(key+':'+value);
+  //   $scope.dd = log.join(',');
+  // },log);
+  
   $scope.submit = function(){
+    console.log($http);
+    console.log($scope.user);
     alert("aaa");
   }
-}]);
+});
